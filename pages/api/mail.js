9c8a0183294538226a4acb3dbce74d7299ff22d8
noahlissner <noah.lissner@gmail.com>
2022-07-07 20:14:00 +0200

    try catch in serverless function

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -2,26 +2,32 @@ const mail = require("@sendgrid/mail");
 
 mail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
 
-export default function handler(req, res) {
-  const body = JSON.parse(req.body);
+export default async function handler(req, res) {
+  try {
+    const body = JSON.parse(req.body);
 
-  const message = `
-  Name: ${body.name}\r\n
-  Email: ${body.email}\r\n
-  Subject: ${body.subject}\r\n
-  Message: ${body.msg}
-  `;
+    const message = `
+    Name: ${body.name}\r\n
+    Email: ${body.email}\r\n
+    Subject: ${body.subject}\r\n
+    Message: ${body.msg}
+    `;
 
-  const data = {
-    to: "hello@lissner.dev",
-    from: "hello@lissner.dev",
-    replyTo: body.email,
-    subject: body.subject,
-    text: message,
-    html: message.replace(/\r\n/g, "<br>"),
-  };
+    const data = {
+      to: "hello@lissner.dev",
+      from: "hello@lissner.dev",
+      replyTo: body.email,
+      subject: body.subject,
+      text: message,
+      html: message.replace(/\r\n/g, "<br>"),
+    };
 
-  mail.send(data);
+    await mail.send(data);
 
-  res.status(200).json({ status: "Ok" });
+    res.status(200).json({ status: "Ok" });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
 }
